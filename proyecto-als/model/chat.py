86954f85be84df57fb7a7fa4d21fc500c1927760
jfmacia <jfmacia@esei.uvigo.es>
2020-05-05 12:44:41 +0200

    index.html creado

--- proyecto-als/model/chat.py
+++ proyecto-als/model/chat.py
@@ -5,6 +5,5 @@ from google.appengine.ext import ndb
 
 
 class Chat(ndb.Model):
-    id = ndb.IntegerProperty(required=True)
     usuario1 = ndb.String(indexed=True)
     usuario2 = ndb.StringProperty(indexed=True)
