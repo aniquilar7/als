4f80fa5b7e2058b2794b77cb3688b963e4fd957c
jfmacia <jfmacia@esei.uvigo.es>
2020-05-06 18:17:37 +0200

    Añadida la funcón para ver mensajes dentro de los chats

---
+++ proyecto-als/handlers/mensajes/lista.py
@@ -0,0 +1,20 @@
+import webapp2
+from webapp2_extras import jinja2
+
+from model.mensaje import Mensaje
+
+class ListaMensajesHandler(webapp2.RequestHandler):
+    def get(self):
+        mensajes = Mensaje.recupera_para(self.request)
+
+        valores_plantilla = {
+            "mensajes": mensajes
+        }
+
+        jinja = jinja2.get_jinja2(app=self.app)
+        self.response.write(jinja.render_template("lista_mensajes.html",
+                            **valores_plantilla))
+
+app = webapp2.WSGIApplication([
+    ('/mensajes/lista', ListaMensajesHandler)
+], debug=True)
--- proyecto-als/model/mensaje.py
+++ proyecto-als/model/mensaje.py
@@ -9,4 +9,18 @@ class Mensaje(ndb.Model):
     fecha = ndb.DateTimeProperty(auto_now_add=True, indexed=True)
     texto = ndb.StringProperty(required=True)
     estado = ndb.BooleanProperty(required=True)
-    chat = ndb.KeyProperty(kind=Chat)
+    clave_chat = ndb.KeyProperty(kind=Chat)
+
+    @staticmethod
+    def recupera_para(req):
+        try:
+            id_chat = req.GET["chat"]
+        except:
+            id_chat = ""
+
+        if id_chat:
+            clave_chat = ndb.Key(urlsafe=id_chat)
+            mensajes = Mensaje.query(Mensaje.clave_chat == clave_chat)
+            return mensajes
+        else:
+            print ("ERROR: chat no encontrado")
