387a4630456d8cdb779dfa6cd32de416dcbed3cb
jfmacia <jfmacia@esei.uvigo.es>
2020-05-06 12:52:31 +0200

    Vista para crear nuevo chat creada

--- proyecto-als/handlers/chats/nuevo.py
+++ proyecto-als/handlers/chats/nuevo.py
@@ -1,1 +1,30 @@
+#coding: utf-8
 #Nuevo chat
+
+
+import webapp2
+
+
+from webapp2_extras import jinja2
+
+
+from model.chat import Chat
+
+class NuevoChatHandler(webapp2.RequestHandler):
+    def get(self):
+
+        valores_plantilla = {
+        }
+
+        jinja = jinja2.get_jinja2(app=self.app)
+        self.response.write(jinja.render_template("nuevo_chat.html",
+                            **valores_plantilla))
+
+    def post(self):
+        self.respone.write("Nuevo chat creado")
+
+app = webapp2.WSGIApplication([
+    ('/chats/nuevo', NuevoChatHandler)
+], debug=True)
+
+
